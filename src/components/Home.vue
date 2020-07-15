149f84aa2a1c9854d3dd521228e58fc320b20f5d
chengxxshow <563255034@qq.com>
2020-07-15 10:35:23 +0800

    myinfo logout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,11 @@
         <el-header class="my_el-header">
             <div  class="title_txt">东软云his系统</div>
             <div class="user_txt" >
-              <el-dropdown>
+              <el-dropdown  @command="handleCommand">
                <i class="el-icon-user-solid" style="margin-right: 15px"></i>
                <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>个人信息</el-dropdown-item>
-                    <el-dropdown-item>退出</el-dropdown-item>
+                    <el-dropdown-item command="myinfo">个人信息</el-dropdown-item>
+                    <el-dropdown-item command="logout">退出</el-dropdown-item>
                </el-dropdown-menu>
                 </el-dropdown>
                 <span>欢迎 {{username}}</span>
@@ -17,16 +17,79 @@
         
         <el-main>Main</el-main>
         <el-footer>Footer</el-footer>
-    </el-container>
+
+  <el-dialog title="个人信息" :visible.sync="dialogFormVisible">
+    <el-form :model="form">
+      <el-form-item label="用户名" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" :label-width="formLabelWidth">
+        <el-input v-model="form.psw" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>   
+
+</el-container>
+
+ 
 </template>
 <script>
 export default {
   name: 'Home',
   data () {
     return {
-      username: sessionStorage.getItem("username")
+      username: sessionStorage.getItem("username"),
+      dialogFormVisible: false,
+      form:{
+        name:"",
+        psw:""
+      },
+      formLabelWidth: '120px'
+    }
+  },
+  methods: {
+      handleCommand(command) {
+        //this.$message('click on item ' + command);
+        //退出
+        if(command=="logout"){
+         this.$confirm('确定退出吗?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center: true
+        }).then(() => {
+           this.axios("/user/logOut").then((resp)=>{
+             //退出：清除sessionStorage  ，跳转到登录页面
+              sessionStorage.removeItem("uId")
+              sessionStorage.removeItem("username")
+              sessionStorage.removeItem("uType")
+              sessionStorage.removeItem("Loginflag")
+              this.$router.replace({path:"/"})
+           })
+        })
+      }
+
+      if(command=="myinfo"){
+          this.dialogFormVisible = true
+          //初始个人信息模态框：查询用户信息
+          let uid=sessionStorage.getItem("uId");
+          console.log(uid)
+          var this_=this
+          this.axios("/user/getUserById?id="+uid).then((resp)=>{
+            console.log(this_.form,)
+            this_.form.name=resp.data.username
+            this_.form.psw=resp.data.password
+          })
+      }
+
     }
   }
+  
+
 }
 </script>
 <style>
---
+++ src/components/user/Myinfo.vue
@@ -0,0 +1,16 @@
+<template>
+      <el-form ref="form" :rules="rules" :model="form" label-width="80px" >
+                <h2 >东软云his系统</h2>
+                <el-form-item label="用户名" prop="username">
+                  <el-input v-model="form.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                  <el-input v-model="form.password" ></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                  <el-button type="primary" @click="submitForm('form')">登录</el-button>
+                </el-form-item>
+    </el-form>
+</template>
+
